ebea81097cb0d88c08a10b1f401255f6ffda0eca
juasbhu28 <juasbhu@hotmail.com>
2023-03-23 22:26:28 -0500

    Class05 valueCero

---
+++ Class05-ValorCero/valorcero.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+//Declaro la variable con el identificador Z para que mantenga valores sólo de tipo int
+var z int 
+
+//El valor cero sirve para
+/*
+	false - boleans
+	0 para integers
+	0.0 para floats
+	"" para strings
+	nil para 
+		pointers
+		funciones
+		interfaces
+		slices
+		channels
+		maps
+
+*/
+
+func main() {
+	//Si la variable z no tiene nada asignado su valor va a ser 0
+	fmt.Println(z)
+}
