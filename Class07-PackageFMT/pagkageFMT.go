1e38198d45ed1816713624786d2e1933856f37bb
juasbhu28 <juasbhu@hotmail.com>
2023-03-26 20:46:19 -0500

    Class07 - Package fmt

---
+++ Class07-PackageFMT/pagkageFMT.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+//Declaro la variable con el identificador Z para que mantenga valores sólo de tipo int
+var a int
+var b string = "Programa"
+var c bool
+var d bool = true
+
+func main() {
+	e := 42
+	//String literal interpretado
+	f := "Dice hola mundo."
+	//Raw 
+	g := ``
+	fmt.Println(z)
+}
