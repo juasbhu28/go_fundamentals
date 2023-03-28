acb0e8421ea01b8ce5b0eba42f1de0c48f37025e
juasbhu28 <juasbhu@hotmail.com>
2023-03-27 22:34:13 -0500

    Class07 - Package FMT

--- Class07-PackageFMT/pagkageFMT.go
+++ Class07-PackageFMT/pagkageFMT.go
@@ -3,7 +3,21 @@ package main
 import (
 	"fmt"
 )
-//Declaro la variable con el identificador Z para que mantenga valores sólo de tipo int
+
+/*
+	El paquete fmt nos ayuda con muchas funciones para tipo texto
+	crear archivos, leer archivos, imprimir en pantalla, etc.
+
+	La documentación la encuentro en https://golang.org/pkg/fmt/ o godoc.org/fmt
+
+	func Println(a ...interface{}) (n int, err error)
+		Imprime en pantalla los valores que recibe, separados por un espacio
+		y termina con un salto de linea.
+		Sus parametros son de tipo interface{} que es un tipo generico que significa que puede
+		recibir cualquier tipo de dato.
+		retorna un entero que es la cantidad de bytes escritos y un error que es nil si no hay error.
+*/
+
 var a int
 var b string = "Programa"
 var c bool
@@ -11,9 +25,53 @@ var d bool = true
 
 func main() {
 	e := 42
-	//String literal interpretado
+	//String literal interpretado " " -> No permite agregar comillas simples ni saltos de linea
 	f := "Dice hola mundo."
-	//Raw 
-	g := ``
-	fmt.Println(z)
+	// Raw string literal ` ` -> Permite imprimir saltos de linea
+	g := `El doctor dicer que comer vegatles es
+		 saludable.`
+	
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println("Mi", b, f)
+	fmt.Println(g)
+
+	//Vamos a usar printf para imprimir en pantalla
+	//Hay tres patrones print, printf y println
+	
+	//Print -> Imprime en pantalla los valores que recibe, separados por un espacio
+	//In go the verbs are:
+	//%v	the value in a default format
+	//	when printing structs, the plus flag (%+v) adds field names
+	fmt.Printf("El valor de la variable es: %v\n", a)
+	//resultado es: El valor de la variable es: 0
+	fmt.Printf("El tipo de a es: %T\n", a)
+	//resultado es: El tipo de a es: int
+	fmt.Printf("El valor de la variable es: %v\n", b)
+	//resultado es: El valor de la variable es: Programa
+	fmt.Printf("El tipo de b es: %T\n", b)
+	//resultado es: El tipo de b es: string
+
+	//The difference between %v and %s is that %v prints the value in the default format, and 
+	//%s prints the value as a string.
+	alias gplff="git pull --force-with-lease"
+
 }
+
+/* salida: 
+0
+Programa
+false
+true
+42
+Dice hola mundo.
+Mi Programa Dice hola mundo.
+El doctor dicer que comer vegatles es 
+		 saludable.
+
+*/
+
